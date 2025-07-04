84773016a40dffb13b2246122df97a2bf4399cdd
Mikael Lirbank <mikael@lirbank.com>
2025-07-04 15:25:51 -0700

    Update not-found.tsx

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -2,11 +2,9 @@ import { Link } from "@tanstack/react-router";
 
 export function NotFound(props: React.PropsWithChildren) {
   return (
-    <div className="flex flex-col gap-2 p-2">
+    <div className="flex flex-1 flex-col items-center justify-center gap-2 p-2">
       <div>
-        {props.children ?? (
-          <div>The page you are looking for does not exist.</div>
-        )}
+        {props.children ?? "The page you are looking for does not exist."}
       </div>
       <div className="flex gap-2">
         <Link to="/" className="underline">
