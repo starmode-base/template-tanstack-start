ee9432e30b557863216e8e6a13871109bdac9c0d
Mikael Lirbank <mikael@lirbank.com>
2025-07-13 10:31:05 -0700

    Port

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import viteReact from "@vitejs/plugin-react";
+import metadata from "./metadata.json";
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: metadata.dev.port,
   },
   plugins: [
     tsConfigPaths({
