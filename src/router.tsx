192ce3a4ea55029285b7ff9ee8550bdf129d69c2
Mikael Lirbank <mikael@lirbank.com>
2025-07-15 10:59:53 -0700

    DefaultCatchBoundary

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { DefaultCatchBoundary } from "./components/default-catch-boundary";
 import { NotFound } from "./components/not-found";
 
 export function createRouter() {
   const router = createTanStackRouter({
     routeTree,
     defaultPreload: "intent",
+    defaultErrorComponent: DefaultCatchBoundary,
     defaultNotFoundComponent: () => <NotFound />,
     scrollRestoration: true,
   });
