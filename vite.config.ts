78d616298d864cada0db7c0308737726aa429117
Mikael Lirbank <mikael@lirbank.com>
2025-08-08 15:27:15 -0700

    Tailwind Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import metadata from "./metadata.json";
 
 export default defineConfig({
@@ -9,10 +10,9 @@ export default defineConfig({
     port: metadata.dev.port,
   },
   plugins: [
-    tsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
+    tsConfigPaths({ projects: ["./tsconfig.json"] }),
     tanstackStart({ customViteReactPlugin: true }),
     viteReact(),
+    tailwindcss(),
   ],
 });
