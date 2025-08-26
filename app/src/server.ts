f5597bf32014f46214261b9ce9521c92182d3e02
Mikael Lirbank <mikael@lirbank.com>
2025-08-26 13:24:13 -0700

    Revert "Upgrade Clerk"
    This reverts commit 78c70144ac744b20ae860327e646defbbca6f1c0.

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,20 +1,11 @@
 import {
   createStartHandler,
-  defineHandlerCallback,
   defaultStreamHandler,
 } from "@tanstack/react-start/server";
 import { createRouter } from "./router";
 import { createClerkHandler } from "@clerk/tanstack-react-start/server";
 
-// https://github.com/clerk/javascript/blob/main/packages/tanstack-react-start/CHANGELOG.md#clerktanstack-react-start
-const handlerFactory = createClerkHandler(
-  createStartHandler({
-    createRouter,
-  }),
-);
-
-export default defineHandlerCallback(async (event) => {
+export default createClerkHandler(
+  createStartHandler({ createRouter }),
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  const startHandler = await handlerFactory(defaultStreamHandler); // awaited
-  return startHandler(event);
-});
+)(defaultStreamHandler);
