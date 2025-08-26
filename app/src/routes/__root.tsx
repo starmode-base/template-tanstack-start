90a417fa7a9a8482920739eaf8056f60d7dc731a
Mikael Lirbank <mikael@lirbank.com>
2025-08-25 18:27:52 -0700

    app template

---
+++ app/src/routes/__root.tsx
@@ -0,0 +1,42 @@
+import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import appCss from "~/styles/app.css?url";
+import metadata from "../../metadata.json";
+import { inject } from "@vercel/analytics";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      { charSet: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { title: metadata.name },
+      { name: "description", content: metadata.description },
+      { name: "og:title", content: metadata.name },
+      { name: "og:description", content: metadata.description },
+      { name: "og:image", content: metadata.shareCardImage },
+    ],
+    links: [
+      { rel: "stylesheet", href: appCss },
+      { rel: "icon", href: `${metadata.browserIcon}?x7k9m2p4` },
+    ],
+  }),
+  shellComponent: RootDocument,
+});
+
+function RootDocument(props: React.PropsWithChildren) {
+  // https://vercel.com/docs/analytics/quickstart
+  inject();
+
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {props.children}
+        <TanStackRouterDevtools position="bottom-right" />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
