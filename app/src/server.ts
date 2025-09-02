ec14a4998b6552767db922142bbf359ef904736f
Mikael Lirbank <mikael@lirbank.com>
2025-09-02 16:01:53 -0700

    Upgrade Clerk

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,15 +1,24 @@
+/**
+ * https://clerk.com/docs/quickstarts/tanstack-react-start
+ * https://github.com/clerk/javascript/blob/main/packages/tanstack-react-start/CHANGELOG.md
+ * https://www.npmjs.com/package/@clerk/tanstack-react-start
+ */
 import {
   createStartHandler,
   defaultStreamHandler,
+  defineHandlerCallback,
 } from "@tanstack/react-start/server";
-import { createClerkHandler } from "@clerk/tanstack-react-start/server";
 import { createRouter } from "./router";
+import { createClerkHandler } from "@clerk/tanstack-react-start/server";
 
-const handler = createStartHandler({
-  createRouter,
+const handlerFactory = createClerkHandler(
+  createStartHandler({
+    createRouter,
+  }),
+);
+
+export default defineHandlerCallback(async (event) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const startHandler = await handlerFactory(defaultStreamHandler);
+  return startHandler(event);
 });
-
-const clerkHandler = createClerkHandler(handler);
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-export default clerkHandler(defaultStreamHandler);
