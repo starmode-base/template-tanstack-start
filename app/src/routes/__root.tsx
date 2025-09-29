88e9407402c902e0f9c3bcdcbcb7b8c1beca3e21
Mikael Lirbank <mikael@lirbank.com>
2025-09-29 07:58:51 -0700

    Auth

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -3,7 +3,13 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import appCss from "~/styles/app.css?url";
 import metadata from "../../metadata.json";
 import { inject } from "@vercel/analytics";
-import { ClerkProvider } from "@clerk/tanstack-react-start";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/tanstack-react-start";
 import { createServerFn } from "@tanstack/react-start";
 import { syncViewer } from "~/auth/clerk";
 
@@ -58,5 +64,45 @@ function Providers(props: React.PropsWithChildren) {
 }
 
 function Shell(props: React.PropsWithChildren) {
-  return <>{props.children}</>;
+  return (
+    <main className="flex h-dvh flex-col">
+      <SignedIn>{props.children}</SignedIn>
+      <SignedOut>
+        <div className="m-auto flex flex-col rounded bg-white shadow">
+          <div className="flex flex-col gap-2 p-4 text-center">
+            <div className="text-2xl font-semibold text-slate-900">
+              TanStack Start template for apps
+            </div>
+            <div className="text-slate-600">
+              A modern, full-stack React application starter
+            </div>
+            <div className="text-sm text-slate-500">
+              Built with TanStack Router, Clerk Auth, and more
+            </div>
+            <div>
+              <a
+                href="https://github.com/starmode-base/template-tanstack-start"
+                target="_blank"
+                className="text-sky-500 underline hover:text-sky-600"
+              >
+                Get the template →
+              </a>
+            </div>
+          </div>
+          <div className="flex gap-2 bg-slate-100 p-4">
+            <SignInButton mode="modal">
+              <button className="rounded bg-sky-500 px-4 py-1 text-white">
+                Sign in
+              </button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <button className="rounded bg-sky-500 px-4 py-1 text-white">
+                Sign up
+              </button>
+            </SignUpButton>
+          </div>
+        </div>
+      </SignedOut>
+    </main>
+  );
 }
--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,16 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignOutButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/tanstack-react-start";
+import { SignOutButton, UserButton } from "@clerk/tanstack-react-start";
+import { createServerFn } from "@tanstack/react-start";
+import { ensureViewerMiddleware } from "~/middleware/auth-middleware";
+
+const authorizedSF = createServerFn()
+  .middleware([ensureViewerMiddleware])
+  .handler(() => {
+    return "ok";
+  });
 
 export const Route = createFileRoute("/")({
   component: Home,
-  loader: ({ context }) => {
+  loader: async ({ context }) => {
+    // This is just to test that the middleware is working
+    const authorized = await authorizedSF();
+    console.log(authorized);
+
     return context.viewer;
   },
 });
@@ -19,37 +24,18 @@ function Home() {
   const viewer = Route.useLoaderData();
 
   return (
-    <main className="flex h-dvh flex-col">
-      <SignedIn>
-        <div className="flex items-center justify-between gap-4 border-b border-slate-200 bg-white p-4">
-          <div>You are signed in as: {viewer?.email}</div>
-          <div className="flex items-center gap-2">
-            <UserButton />
-            <SignOutButton>
-              <button className="h-fit rounded bg-sky-500 px-4 py-1 text-white">
-                Sign out
-              </button>
-            </SignOutButton>
-          </div>
+    <>
+      <div className="flex items-center justify-between gap-4 border-b border-slate-200 bg-white p-4">
+        <div>You are signed in as: {viewer?.email}</div>
+        <div className="flex items-center gap-2">
+          <UserButton />
+          <SignOutButton>
+            <button className="h-fit rounded bg-sky-500 px-4 py-1 text-white">
+              Sign out
+            </button>
+          </SignOutButton>
         </div>
-      </SignedIn>
-      <SignedOut>
-        <div className="m-auto flex flex-col gap-4 rounded bg-white p-4 shadow">
-          <div className="text-center">You are signed out</div>
-          <div className="flex gap-2">
-            <SignInButton mode="modal">
-              <button className="rounded bg-sky-500 px-4 py-1 text-white">
-                Sign in
-              </button>
-            </SignInButton>
-            <SignUpButton mode="modal">
-              <button className="rounded bg-sky-500 px-4 py-1 text-white">
-                Sign up
-              </button>
-            </SignUpButton>
-          </div>
-        </div>
-      </SignedOut>
+      </div>
       <div className="flex flex-1">
         <div className="m-auto flex flex-col gap-2 pb-10 text-center">
           <div className="text-2xl font-semibold text-slate-900">
@@ -72,6 +58,6 @@ function Home() {
           </div>
         </div>
       </div>
-    </main>
+    </>
   );
 }
